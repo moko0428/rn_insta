393f0004b97ee6e7cdcacac8c439a307328c92f1
moko <ljun925@naver.com>
2024-03-23 03:27:50 +0900

    feat: Home - story, feed 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,286 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 
+const logo = require('../assets/icons/home/logo.png');
+const likeIcon = require('../assets/icons/home/likeIcon.png');
+const commentIcon = require('../assets/icons/home/commentIcon.png');
+const moreIcon = require('../assets/icons/home/more.png');
+
+const {width} = Dimensions.get('window');
+
+const dummy_story = [
+  {
+    id: 1,
+    name: 'feb.25jy',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+  {
+    id: 2,
+    name: 'SONGSONGSONGSONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+  {
+    id: 3,
+    name: 'NONGNONGNONGNONGNONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: false,
+  },
+  {
+    id: 4,
+    name: 'DONGDONGDONGDONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: false,
+  },
+  {
+    id: 5,
+    name: 'BONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: false,
+  },
+  {
+    id: 6,
+    name: 'SONGSONGSONGSONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+  {
+    id: 7,
+    name: 'NONGNONGNONGNONGNONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+  {
+    id: 8,
+    name: 'DONGDONGDONGDONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: false,
+  },
+  {
+    id: 9,
+    name: 'BONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+  {
+    id: 10,
+    name: 'SONGSONGSONGSONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: false,
+  },
+  {
+    id: 11,
+    name: 'NONGNONGNONGNONGNONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+  {
+    id: 12,
+    name: 'DONGDONGDONGDONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    isNew: true,
+  },
+];
+
+const dummy_feed = [
+  {
+    id: 1,
+    name: 'feb.25jy',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    feedImg: [
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+    ],
+    contents: '내 마음... 받아줘',
+    like: 37,
+    likeUsers: [1, 2, 3],
+  },
+  {
+    id: 2,
+    name: 'BONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    feedImg: [
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+    ],
+    contents: '내 마음... 받아줘',
+    like: 37,
+    likeUsers: [1, 2, 3],
+  },
+  {
+    id: 3,
+    name: 'SONG',
+    profileImg: 'https://avatar.iran.liara.run/public',
+    feedImg: [
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+      'https://picsum.photos/400/400',
+    ],
+    contents: '내 마음... 받아줘',
+    like: 37,
+    likeUsers: [1, 2, 3],
+  },
+];
 export default () => {
+  const renderStory = ({item, index}) => {
+    return (
+      <TouchableOpacity
+        style={index === 0 ? {marginHorizontal: 16} : {marginRight: 16}}>
+        <View style={{alignItems: 'center'}}>
+          <Image
+            source={{uri: item.profileImg}}
+            style={
+              item.isNew
+                ? {width: 52, height: 52, marginBottom: 2}
+                : {
+                    width: 52,
+                    height: 52,
+                    marginBottom: 2,
+                    borderWidth: 2,
+                    borderRadius: 100,
+                    borderColor: '#2a85ff',
+                  }
+            }
+          />
+          <Text
+            numberOfLines={1}
+            style={{
+              maxWidth: 52,
+              fontSize: 13,
+              fontWeight: '400',
+              lineHeight: 16.22,
+              color: '#4f4f4f',
+            }}>
+            {item.name}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  const renderFeed = ({item, index}) => {
+    return (
+      <View style={{paddingVertical: 24}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginHorizontal: 16,
+            marginBottom: 8,
+          }}>
+          <TouchableOpacity
+            style={{flexDirection: 'row', alignItems: 'center', gap: 4}}>
+            <Image
+              source={{uri: item.profileImg}}
+              style={{width: 32, height: 32}}
+            />
+            <Text style={{fontSize: 16, fontWeight: '400', lineHeight: 19.97}}>
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={moreIcon} style={{width: 24, height: 24}} />
+          </TouchableOpacity>
+        </View>
+        <Image
+          source={{uri: item.feedImg[0]}}
+          style={{width: width, height: width, marginBottom: 8}}
+          resizeMode="contain"
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            paddingHorizontal: 16,
+            marginBottom: 32,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 8,
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity>
+              <Image source={likeIcon} style={{width: 32, height: 32}} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image source={commentIcon} style={{width: 32, height: 32}} />
+            </TouchableOpacity>
+          </View>
+          <Text>외 {item.like}명이 좋아합니다.</Text>
+        </View>
+        <View style={{marginHorizontal: 16, gap: 4}}>
+          <Text>{item.name}</Text>
+          <Text style={{fontWeight: '400', color: '#4f4f4f'}}>
+            {item.contents}
+          </Text>
+        </View>
+      </View>
+    );
+  };
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-      <View>
-        <Text>Home</Text>
+      <View style={{flex: 1, backgroundColor: '#fff', marginBottom: 32}}>
+        <FlatList
+          data={dummy_feed}
+          renderItem={renderFeed}
+          keyExtractor={item => item.id}
+          removeClippedSubviews
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={() => (
+            <View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  padding: 16,
+                  alignItems: 'center',
+                }}>
+                <View>
+                  <Image source={logo} style={{width: 88, height: 21.91}} />
+                </View>
+                <View
+                  style={{flexDirection: 'row', gap: 8, alignItems: 'center'}}>
+                  <TouchableOpacity>
+                    <Image source={likeIcon} style={{width: 32, height: 32}} />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Image
+                      source={commentIcon}
+                      style={{width: 32, height: 32}}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View>
+                <FlatList
+                  data={dummy_story}
+                  renderItem={renderStory}
+                  keyExtractor={item => item.id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  removeClippedSubviews
+                />
+              </View>
+            </View>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
