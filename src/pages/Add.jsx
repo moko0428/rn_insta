508f5161db785d6f130a73855183e24db94711e0
moko <ljun925@naver.com>
2024-03-25 17:04:12 +0900

    feat: 새 게시물 페이지 구현

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,11 +1,116 @@
-import {SafeAreaView, Text, View} from 'react-native';
-import React from 'react';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {CameraRoll} from '@react-native-camera-roll/camera-roll';
 
-export default () => {
+export default ({navigation}) => {
+  const {width, height} = useWindowDimensions();
+  const [images, setImages] = useState([]);
+  const [selectedPhoto, setSelectedPhoto] = useState();
+  const [selectedIndex, setSelectedIndex] = useState();
+
+  useEffect(() => {
+    FetchImages();
+  }, []);
+
+  const FetchImages = async () => {
+    CameraRoll.getPhotos({
+      first: 100,
+      assetType: 'Photos',
+      groupTypes: 'All',
+    }).then(res => {
+      if (!selectedPhoto) {
+        setSelectedPhoto(res.edges[0].node.image);
+        setSelectedIndex(0);
+      }
+      setImages(res.edges.map(e => e.node.image));
+    });
+  };
+
+  const renderItem = ({item, index}) => {
+    return (
+      <TouchableOpacity
+        style={{borderWidth: 1, borderColor: '#fff'}}
+        onPress={() => {
+          setSelectedPhoto(item);
+          setSelectedIndex(index);
+        }}>
+        {selectedIndex === index && (
+          <View
+            style={{
+              position: 'absolute',
+              width: 20,
+              height: 20,
+              right: 8,
+              top: 2,
+              borderWidth: 1,
+              borderColor: '#000',
+              borderRadius: 20,
+              zIndex: 2,
+            }}>
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                borderRadius: 10,
+                backgroundColor: 'red',
+              }}
+            />
+          </View>
+        )}
+        <Image
+          source={item}
+          style={{width: width / 4 - 2, height: width / 4 - 2}}
+        />
+      </TouchableOpacity>
+    );
+  };
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-      <View>
-        <Text>Add</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#000'}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginHorizontal: 16,
+          marginBottom: 8,
+        }}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={{fontSize: 17, color: '#fff', fontWeight: 'bold'}}>
+            X
+          </Text>
+        </TouchableOpacity>
+        <Text style={{fontSize: 15, color: '#fff', fontWeight: 'bold'}}>
+          새 게시물
+        </Text>
+        <TouchableOpacity>
+          <Text style={{fontSize: 14, color: '#fff', fontWeight: 'bold'}}>
+            다음
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{backgroundColor: '#000', flex: 0.5}}>
+        <Image
+          source={{uri: selectedPhoto?.uri}}
+          style={{width: '100%', height: '100%'}}
+        />
+      </View>
+      <View style={{flex: 0.5}}>
+        <FlatList
+          data={images}
+          renderItem={renderItem}
+          keyExtractor={item => item.uri}
+          numColumns={4}
+          showsVerticalScrollIndicator={false}
+          removeClippedSubviews
+        />
       </View>
     </SafeAreaView>
   );
